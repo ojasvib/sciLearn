40a2dcb234f41ebd1d0eaab4acae608879219c50
ojasvibhargava-ml <43949526+ojasvibhargava-ml@users.noreply.github.com>
2019-10-10 11:57:11 +0530

    Update and rename ridgeRegression to ridgeRegression.py

--- Regression/ridgeRegression.py
+++ Regression/ridgeRegression.py
@@ -1,4 +1,4 @@
-
+#Train an l2 regularized(ridgeRegression) linear regression model on the datase, report coefficients
 from sklearn import linear_model
 reg = linear_model.RidgeCV(alphas=[10])
 X=np.genfromtxt('LR_X.csv',delimiter=',')
