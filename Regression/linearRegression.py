fd5c61eff3cb75c16781b3a5cedcac094549beec
ojasvibhargava-ml <43949526+ojasvibhargava-ml@users.noreply.github.com>
2019-10-10 11:55:02 +0530

    Update and rename linearRegression to linearRegression.py

--- Regression/linearRegression.py
+++ Regression/linearRegression.py
@@ -1,3 +1,5 @@
+#Train a linear regression model on the dataset(LR_X.csv,LR_Y.csv). Report the coefficients
+
 from sklearn import linear_model
 reg = linear_model.LinearRegression()
 X=np.genfromtxt('LR_X.csv',delimiter=',')
